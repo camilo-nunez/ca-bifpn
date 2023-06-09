de6674237eb2634660e04e2001eec8e99221eb42
cnunezf <camilo.nunezf@sansano.usm.cl>
2023-06-09 19:35:15 +0000

    minor typos

--- model/builder_fpn.py
+++ model/builder_fpn.py
@@ -1,8 +1,17 @@
 from .bifpn.model import BiFPN
+from .cabifpn.model import CABiFPN
 
-def builder_bifpn(bifpn_config, backbone_config):
-    
-    return [BiFPN(bifpn_config.NUM_CHANNELS, 
-                  backbone_config.IN_CHANNELS, 
-                  first_time=True if _ == 0 else False)
-            for _ in range(bifpn_config.NUM_LAYERS)]
+def builder_fpn(base_config):
+    if base_config.MODEL.BIFPN.TYPE == 'BS':
+        fpn_buider = BiFPN
+        print('[+] BiFPN loaded')
+    elif base_config.MODEL.BIFPN.TYPE == 'CA':
+        fpn_buider = CABiFPN
+        print('[+] CABiFPN loaded')
+    else:
+        raise RuntimeError("[+] FPN backbone does not exist !")
+            
+    return [fpn_buider(base_config.MODEL.BIFPN.NUM_CHANNELS,
+                       base_config.MODEL.BACKBONE.IN_CHANNELS, 
+                       first_time=True if _ == 0 else False)
+            for _ in range(base_config.MODEL.BIFPN.NUM_LAYERS)]
