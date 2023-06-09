0fb240abd1d0c6264b82a98c4e288bc002169ee8
cnunezf <camilo.nunezf@sansano.usm.cl>
2023-06-09 22:41:52 +0000

    update typo

--- model/builder_fpn.py
+++ model/builder_fpn.py
@@ -2,14 +2,14 @@ from .bifpn.model import BiFPN
 from .cabifpn.model import CABiFPN
 
 def builder_fpn(base_config):
-    if base_config.MODEL.BIFPN.TYPE == 'BS':
+    if base_config.MODEL.BIFPN.TYPE == 'bs':
         fpn_buider = BiFPN
         print('[+] BiFPN loaded')
-    elif base_config.MODEL.BIFPN.TYPE == 'CA':
+    elif base_config.MODEL.BIFPN.TYPE == 'ca':
         fpn_buider = CABiFPN
         print('[+] CABiFPN loaded')
     else:
-        raise RuntimeError("[+] FPN backbone does not exist !")
+        raise RuntimeError("[+] FPN backbone does not exist !. Select the FPN backbone, this should be \'bs\' (refer to baseline) or \'ca\' (refer to context agregation.")
             
     return [fpn_buider(base_config.MODEL.BIFPN.NUM_CHANNELS,
                        base_config.MODEL.BACKBONE.IN_CHANNELS, 
