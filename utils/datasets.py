572691ae935979b01b6e6067062fab14922e7c72
cnunezf <camilo.nunezf@sansano.usm.cl>
2023-06-11 02:58:36 +0000

    coco vars image_id, area and iscrowd

--- utils/datasets.py
+++ utils/datasets.py
@@ -17,14 +17,20 @@ class VOCDetectionV2(VOCDetection):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         target = self.parse_voc_xml(ET_parse(self.annotations[index]).getroot())
         target = self.convert_target(target)
-        
+
         if not isinstance(self.transform, A.core.composition.Compose): RuntimeError("[+] The transform compose must by an Albumentations's type.!")
         if self.transform is not None:
             transformed = self.transform(image=np.asarray(img), bboxes=target['boxes'], labels=target['labels'])
             img = transformed['image']
             target['boxes'] = torch.as_tensor(transformed['bboxes'], dtype=torch.float32)
             target['labels'] = torch.as_tensor(transformed['labels'], dtype=torch.int64)
-
+        
+        ## COCO vars
+        target["image_id"] = torch.tensor([index])
+        target["area"] = (target['boxes'][:, 3] - target['boxes'][:, 1]) * (target['boxes'][:, 2] - target['boxes'][:, 0])
+        # suppose all instances are not crowd
+        target["iscrowd"] = torch.zeros((len(target['labels']),), dtype=torch.int64)
+        
         return img, target
 
     def convert_target(self,target):
@@ -42,5 +48,5 @@ class VOCDetectionV2(VOCDetection):
         new_target = {}
         new_target["boxes"] = boxes
         new_target["labels"] = labels
-
+        
         return new_target
