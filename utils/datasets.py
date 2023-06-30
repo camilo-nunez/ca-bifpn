b6473f783eb065ced55e8e0656d3b89b4833a093
cnunezf <camilo.nunezf@sansano.usm.cl>
2023-06-30 21:21:44 +0000

    cocodataset v2 available

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,9 +1,14 @@
 import torch
-from torchvision.datasets import VOCDetection #https://pytorch.org/vision/0.15/_modules/torchvision/datasets/voc.html
+from torchvision.datasets import VOCDetection, CocoDetection
+from torchvision.ops import box_convert
 
 import cv2
+import copy
+import os
 import albumentations as A
 import numpy as np
+
+from typing import Any, Callable, List, Optional, Tuple
 
 try:
     from defusedxml.ElementTree import parse as ET_parse
@@ -12,7 +17,7 @@ except ImportError:
 
 class VOCDetectionV2(VOCDetection):
 
-    def __getitem__(self, index: int, transform=None):
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
         img = cv2.imread(self.images[index])
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         target = self.parse_voc_xml(ET_parse(self.annotations[index]).getroot())
@@ -50,3 +55,90 @@ class VOCDetectionV2(VOCDetection):
         new_target["labels"] = labels
         
         return new_target
+    
+class CocoDetectionV2(CocoDetection):
+    
+    def _has_only_empty_bbox(self, anno):
+        return all(any(o <= 1 for o in obj["bbox"][2:]) for obj in anno)
+
+    def _count_visible_keypoints(self, anno):
+        return sum(sum(1 for v in ann["keypoints"][2::3] if v > 0) for ann in anno)
+    
+    def _has_less_or_equal_bbox(self, anno):
+        return all(((obj['bbox'][0] + obj['bbox'][2] <= obj['bbox'][0]) and (obj['bbox'][1] + obj['bbox'][3] <= obj['bbox'][1])) for obj in anno)
+
+    min_keypoints_per_image = 15
+
+    def _has_valid_annotation(self, anno):
+        # if it's empty, there is no annotation
+        if len(anno) == 0:
+            return False
+        # if all boxes have close to zero area, there is no annotation
+        if self._has_only_empty_bbox(anno):
+            return False
+        # if x_max/y_max is less than or equal to x_min/y_min for bbox
+        if self._has_less_or_equal_bbox(anno):
+            return False
+        # keypoints task have a slight different criteria for considering
+        # if an annotation is valid
+        if "keypoints" not in anno[0]:
+            return True
+        # for keypoint detection tasks, only consider valid images those
+        # containing at least min_keypoints_per_image
+        if self._count_visible_keypoints(anno) >= min_keypoints_per_image:
+            return True
+        return False
+    
+    def __init__(self,
+        root: str,
+        annFile: str,
+        transform: Optional[Callable] = None
+    ) -> None:
+        super().__init__(root, annFile, transform)
+        
+        self.ids_n = []
+        coco_a = copy.deepcopy(self.coco)
+        for ds_idx, img_id in enumerate(self.ids):
+            ann_ids = coco_a.getAnnIds(imgIds=img_id, iscrowd=None)
+            anno = coco_a.loadAnns(ann_ids)
+            if self._has_valid_annotation(anno):
+                self.ids_n.append(img_id)
+
+    def _load_image(self, id: int):
+        path = self.coco.loadImgs(id)[0]["file_name"]
+        img = cv2.imread(os.path.join(self.root, path))
+        return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        id = self.ids_n[index]
+        
+        image = self._load_image(id)
+        target = self._load_target(id)
+        
+        n_target = dict()
+        for t in target:
+            for k,v in t.items():
+                if k not in n_target: n_target[k] = []
+                n_target[k].append(v)
+        
+        n_target["bbox"] = torch.as_tensor(n_target["bbox"], dtype=torch.float32)
+        n_target["bbox"] = box_convert(n_target["bbox"], in_fmt='xywh', out_fmt='xyxy')
+        
+        f_target = dict()
+        if self.transform is not None:
+            if not isinstance(self.transform, A.core.composition.Compose): RuntimeError("[+] The transform compose must by an Albumentations's type.!")
+            transformed = self.transform(image=np.asarray(image), bboxes=n_target['bbox'], category_ids=n_target['category_id'])
+            image = transformed['image']
+            f_target["boxes"] = torch.as_tensor(transformed['bboxes'], dtype=torch.float32)
+            f_target["labels"] = torch.as_tensor(transformed['category_ids'], dtype=torch.int64)
+        else:
+            f_target["boxes"] = torch.as_tensor(n_target['bbox'], dtype=torch.float32)
+            f_target["labels"] = torch.as_tensor(n_target['category_id'], dtype=torch.int64)
+        f_target["image_id"] = torch.tensor([index])
+        f_target["area"] = torch.as_tensor(n_target['area'])
+        f_target["iscrowd"] = torch.as_tensor(n_target['iscrowd'])
+
+        return image, f_target
+
+    def __len__(self) -> int:
+        return len(self.ids_n)
